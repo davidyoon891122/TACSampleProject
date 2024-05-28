aa781eff01208aba79eae681b4a97eb252329955
David Yoon <davidyoon@hanwha.com>
2024-05-28 16:19:31 +0900

    Initial Commit

---
+++ TCATestSample/TCATestSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATestSampleApp.swift
+//  TCATestSample
+//
+//  Created by Davidyoon on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TCATestSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
