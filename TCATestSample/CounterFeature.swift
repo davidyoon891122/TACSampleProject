18a15f53a5eedcd5506195a559066d8145020531
David Yoon <davidyoon@hanwha.com>
2024-05-29 08:31:35 +0900

    Add an Effect
    * Added a Effect to handle async task
    * Added a action to handle response from the effect

--- TCATestSample/CounterFeature.swift
+++ TCATestSample/CounterFeature.swift
@@ -13,11 +13,15 @@ struct CounterFeature {
     @ObservableState
     struct State {
         var count = 0
+        var fact: String?
+        var isLoading = false
     }
     
     enum Action {
         case descrementButtonTapped
         case incrementButtonTapped
+        case factButtonTapped
+        case factResponse(String)
     }
     
     var body: some ReducerOf<Self> {
@@ -25,9 +29,25 @@ struct CounterFeature {
             switch action {
             case .descrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
                 return .none
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
+                return .none
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared
+                        .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact))
+                }
+            case .factResponse(let fact):
+                state.fact = fact
+                state.isLoading = false
                 return .none
             }
         }
@@ -60,6 +80,22 @@ struct CounterView: View {
                 .padding()
                 .background(.black.opacity(0.1))
                 .clipShape(.rect(cornerRadius: 10))
+            }
+            Button("Fact") {
+                store.send(.factButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(.black.opacity(0.1))
+            .clipShape(.rect(cornerRadius: 10))
+            
+            if store.isLoading {
+                ProgressView()
+            } else if let fact = store.fact {
+                Text(fact)
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .padding()
             }
         }
     }
