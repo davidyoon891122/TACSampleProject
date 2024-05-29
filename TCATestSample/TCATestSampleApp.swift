c8f774472f068f9317c1b4fc9c3c27aeba8f2ead
David Yoon <davidyoon@hanwha.com>
2024-05-29 14:51:33 +0900

    Replace the Separated CounterStore to AppStore
    * Replaced the store1, 2 to the appFeature store using the Scope
    * Added an AppFeatureTests to test AppFeature Reducer

--- TCATestSample/TCATestSampleApp.swift
+++ TCATestSample/TCATestSampleApp.swift
@@ -10,16 +10,14 @@ import ComposableArchitecture
 
 @main
 struct TCATestSampleApp: App {
-    static let store = Store(initialState: CounterFeature.State()) {
-        CounterFeature()
+    static let store = Store(initialState: AppFeature.State()) {
+        AppFeature()
             ._printChanges()
     }
     
     var body: some Scene {
         WindowGroup {
-            CounterView(
-                store: TCATestSampleApp.store
-            )
+            AppView(store: TCATestSampleApp.store)
         }
     }
 }
