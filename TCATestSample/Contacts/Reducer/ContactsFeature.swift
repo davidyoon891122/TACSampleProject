b80e80461aa8883bf6c914a8581c4c47273dc7aa
David Yoon <davidyoon@hanwha.com>
2024-05-29 17:35:58 +0900

    Apply the Destination reducer
    * Added a destination reducer to integrate multiple models
    * Replaced the separated action to destination
    * Replaced the separated Presenteds to the destination

--- TCATestSample/Contacts/Reducer/ContactsFeature.swift
+++ TCATestSample/Contacts/Reducer/ContactsFeature.swift
@@ -18,15 +18,13 @@ struct ContactsFeature {
     
     @ObservableState
     struct State: Equatable {
-        @Presents var addContact: AddContactFeature.State?
-        @Presents var alert: AlertState<Action.Alert>?
         var contacts: IdentifiedArrayOf<Contact> = []
+        @Presents var destination: Destination.State?
     }
     
     enum Action {
         case addButtonTapped
-        case addContact(PresentationAction<AddContactFeature.Action>)
-        case alert(PresentationAction<Alert>)
+        case destination(PresentationAction<Destination.Action>)
         case deleteButtonTapped(id: Contact.ID)
         
         enum Alert: Equatable {
@@ -39,39 +37,48 @@ struct ContactsFeature {
             
             switch action {
             case .addButtonTapped:
-                state.addContact = AddContactFeature.State(
-                    contact: Contact(id: UUID(), name: "")
+                state.destination = .addContact(
+                    AddContactFeature.State(
+                        contact: Contact(id: UUID(), name: "")
+                    )
                 )
                 return .none
-            case let .addContact(.presented(.delegate(.saveContact(contact)))):
+                
+            case let .destination(.presented(.addContact(.delegate(.saveContact(contact))))):
                 state.contacts.append(contact)
                 return .none
                 
-            case let .alert(.presented(.confirmDeletion(id: id))):
+            case let .destination(.presented(.alert(.confirmDeletion(id: id)))):
                 state.contacts.remove(id: id)
                 return .none
                 
-            case .alert:
+            case .destination:
                 return .none
                 
-            case .addContact:
-                return .none
             case .deleteButtonTapped(id: let id):
-                state.alert = AlertState {
-                    TextState("Are you sure?")
-                } actions: {
-                    ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
-                        TextState("Delete")
+                state.destination = .alert(
+                    AlertState {
+                        TextState("Are you sure?")
+                    } actions: {
+                        ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
+                            TextState("Delete")
+                        }
                     }
-                }
+                )
                 
                 return .none
             }
         }
-        .ifLet(\.$addContact, action: \.addContact) {
-            AddContactFeature()
-        }
-        .ifLet(\.alert, action: \.alert)
+        .ifLet(\.$destination, action: \.destination)
+    }
+    
+}
+
+extension ContactsFeature {
+    @Reducer(state: .equatable)
+    enum Destination {
+        case addContact(AddContactFeature)
+        case alert(AlertState<ContactsFeature.Action.Alert>)
     }
     
 }
@@ -107,12 +114,14 @@ struct ContactsView: View {
                 }
             }
         }
-        .sheet(item: $store.scope(state: \.addContact, action: \.addContact)) { addContactStore in
+        .sheet(
+            item: $store.scope(state: \.destination?.addContact, action: \.destination.addContact)
+        ) { addContactStore in
             NavigationStack {
                 AddContactView(store: addContactStore)
             }
         }
-        .alert($store.scope(state: \.alert, action: \.alert))
+        .alert($store.scope(state: \.destination?.alert, action: \.destination.alert))
     }
     
 }
