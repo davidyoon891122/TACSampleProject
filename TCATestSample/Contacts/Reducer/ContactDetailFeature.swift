e07485e73f68bad97561fdfde3509547c313a2b3
David Yoon <davidyoon@hanwha.com>
2024-05-30 09:40:34 +0900

    Create a ContactDetailFeature
    * Added a Reducer
    * Added a ContactDetailView

---
+++ TCATestSample/Contacts/Reducer/ContactDetailFeature.swift
@@ -0,0 +1,51 @@
+//
+//  ContactDetailFeature.swift
+//  TCATestSample
+//
+//  Created by Davidyoon on 5/30/24.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+@Reducer
+struct ContactDetailFeature {
+    @ObservableState
+    struct State: Equatable {
+        let contact: Contact
+    }
+    
+    enum Action {
+        
+    }
+    
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            }
+        }
+    }
+}
+
+struct ContactDetailView: View {
+    
+    let store: StoreOf<ContactDetailFeature>
+    
+    var body: some View {
+        Form {
+            
+        }
+        .navigationTitle(Text(store.contact.name))
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ContactDetailView(store: Store(initialState: ContactDetailFeature.State(
+            contact: Contact(id: UUID(), name: "Blob")
+        )) {
+            ContactDetailFeature()
+        })
+    }
+}
